3c74acb1ea3f434d7e0c3cfc1f37d0299a0f083e
User <user@example.com>
2025-12-09 16:04:49 +0530

    Add fallback for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyALkMT2x4pn6Qc_hJ8LHViPh_tIKXsyC_E",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "mentorphysical-a1003.firebaseapp.com",
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "mentorphysical-a1003",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "mentorphysical-a1003.firebasestorage.app",
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "243840582200",
+    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:243840582200:web:1b28a6edb915d5d70e1ccd",
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-8MQ7NRCYFB"
 };
 
 const app = initializeApp(firebaseConfig);
